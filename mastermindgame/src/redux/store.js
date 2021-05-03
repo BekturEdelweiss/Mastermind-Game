de7c55636855efc172140330594a6336fd482d2c
Bektur Elebesov <bektur.elebesov@gmail.com>
2021-05-03 00:43:11 -0500

    read me file adjusted

--- mastermindgame/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import numbersReducer from "./numbers";
-
-const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
-const store = createStore(numbersReducer, middleware);
-
-export default store;
